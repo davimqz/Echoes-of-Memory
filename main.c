0caad4deac12f7b074474bbd166b202ab41d585b
CaioLira18 <caiozao1212@gmail.com>
2025-11-03 20:55:16 -0300

    [feat] - Função de Andar

--- main.c
+++ main.c
@@ -2,159 +2,59 @@
 #include <stdlib.h>
 #include <raylib.h>
 
-typedef struct Card {
-    int id;
-    int isTurned;
-    int isRemoved;
-    int times_reveled;
-    int lines;
-    int colums;
-} Card;
-
-typedef struct Node {
-    Card card;
-    struct Node *next;  
-} Node;
-
 typedef struct {
-    Node *start;
-    int len;
-} Cortex;
+    float x;
+    float y;
+    int facingRight; 
+} Player;
 
-typedef struct {
-    int lines;
-    int colums;
-    int cell_width;
-    int cell_height;
-    int margin;
-} BoardView;
-
-static int cortex_len (const Cortex* cortex) {
-    if (cortex == NULL) {
-        return 0;
-    } else {
-        return cortex -> len;
+void moveCharacter(Player *player, float speed) {
+    if (IsKeyDown(KEY_A)) {
+        player->x -= speed;
+        player->facingRight = 0;
     }
-}
-
-Node *createNode (Card card) {
-    Node *novo = (Node*)malloc(sizeof(Node));
-    if (!novo) {
-        return NULL;
+    if (IsKeyDown(KEY_D)) {
+        player->x += speed;
+        player->facingRight = 1;
     }
 
-    novo -> card = card;
-    novo -> next = NULL;
-    return novo; 
-}
-
-void cortex_add_init (Cortex* cortex, Card card) {
-    if (!cortex) {
-        return;
-    }
-
-    Node *novo = createNode(card);
-
-    if (!novo) {
-        return;
-    }
-
-    novo -> next = cortex -> start;
-    cortex -> start = novo;
-    cortex -> len += 1;
-}
-
-Node *cortex_search_pos (Cortex* cortex, int pos) {
-    if (!cortex || pos < 0) {
-        return NULL;
-    }
-
-    Node *init = cortex -> start;
-
-    int i = 0;
-
-    while (init && i < pos) {
-        init = init -> next;
-        i++;
-    }
-    return init;
-}
-
-void cortex_swap_pos (Cortex *cortex, int i, int j) {
-    if (!cortex || i == j || i < 0 || j < 0) {
-        return;
-    }
-
-    Node *a = cortex_search_pos(cortex, i);
-    Node *b = cortex_search_pos(cortex, j);
-
-    if (!a || !b) {
-        return;
-    }
-
-    Card temp = a -> card;
-    a -> card = b -> card;
-    b -> card = temp;
-}
-
-void cortex_embaralhar (Cortex *cortex, unsigned seed) {
-    if (!cortex || cortex -> len <= 1) {
-        return;
-    }
-
-    int n = cortex -> len;
-
-    Node **v = (Node**)malloc(sizeof(Node*) * n);
-
-    if (!v) {
-        return;
-    }
-
-    Node *p = cortex -> start;
-
-    for (int i = 0; i < n; i++) {
-        v[i] = p;
-        p = p -> next;
-    }
-
-    srand(seed);
-
-    for (int i = n - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
-        Card temp = v[i] -> card;
-        v[i] -> card = v[j] -> card;
-        v[j] -> card = temp;
-    }
-    free(v);
+    if (player->x < 0) player->x = 0;
+    if (player->x > GetScreenWidth() - 64) player->x = GetScreenWidth() - 64; 
 }
 
 int main() {
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = 1368;   
+    const int screenHeight = 768;
 
-    InitWindow(screenWidth, screenHeight, "Primeiros passos com a Raylib");
+    InitWindow(screenWidth, screenHeight, "Cenario com Boneco");
 
-    Texture sprite = LoadTexture("./assets/character.png");
-    Texture bg = LoadTexture("./assets/cenary1.png");
+    Texture2D spriteLeft = LoadTexture("./assets/characterLeft.png");
+    Texture2D spriteRight = LoadTexture("./assets/characterRight.png");
 
-    // Centraliza o sprite
-    float posx = (screenWidth - sprite.width) / 2.0f;
-    float posy = (1500 - sprite.height) / 2.0f;
+    Player player = { 
+        screenWidth / 2.0f, screenHeight - spriteLeft.height - 30, 1 
+    };
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        moveCharacter(&player, 8.0f);
+
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(RAYWHITE);
 
-        DrawTexture(bg, 0, 0, WHITE);
-        DrawText("Primeiros Passos com a RayLib", 50, 50, 30, BLACK);
+        if (player.facingRight)
+            DrawTexture(spriteRight, player.x, player.y, WHITE);
+        else
+            DrawTexture(spriteLeft, player.x, player.y, WHITE);
 
-        DrawTexture(sprite, posx, posy, WHITE);
+        DrawText("Use A e D para andar", 30, 30, 25, BLACK);
 
         EndDrawing();
     }
 
-    UnloadTexture(sprite);
-    UnloadTexture(bg);
+    UnloadTexture(spriteLeft);
+    UnloadTexture(spriteRight);
     CloseWindow();
 
     return 0;
